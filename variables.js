58b5fec761393af87123c9a8b803b2481d7344e7
nate <nate@nate.gg>
2020-09-12 20:09:52 -0400

    Update variables.js

--- variables.js
+++ variables.js
@@ -1,10 +1,10 @@
 // Discord info
-const discord_bot_token = 'NzEyNzc3OTUyNzA5OTY3OTUy.XsWgMg.scjpAAsf-BD7iP9YnxAVqoStd10';
-const discord_guild_id = '754480323072360498';
-const discord_verified_role = 'Verified';
+const discord_bot_token = '';
+const discord_guild_id = '';
+const discord_verified_role = '';
 
 // reCAPTCHA v2 secret key
-const recaptcha_secret_key = '6Lc2gMsZAAAAAMWhmmfbug23sxl-Vqc-uGPgMyhA';
+const recaptcha_secret_key = '';
 
 module.exports = {
     discord_bot_token,
