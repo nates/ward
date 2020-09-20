2da81e3e68ecce7a1989ad083f354a68d1925a55
nate <nate@nate.gg>
2020-09-19 22:16:39 -0400

    Update discord.js

--- discord.js
+++ discord.js
@@ -4,7 +4,8 @@ const client = new Discord.Client();
 const {
     discord_guild_id,
     discord_bot_token,
-    discord_verified_role
+    discord_verified_role,
+    domain
 } = require('./variables')
 
 client.on('ready', () => {
@@ -15,7 +16,7 @@ client.on('guildMemberAdd', member => {
     const linkId = pool.createLink(member.id);
     const embed = new Discord.MessageEmbed()
         .setTitle('reCAPTCHA Verification')
-        .setDescription(`To gain access to this server you must solve a captcha. The link will expire in 15 minutes.\nhttp://localhost:8080/verify/${linkId}`)
+        .setDescription(`To gain access to this server you must solve a captcha. The link will expire in 15 minutes.\nhttp://${domain}:8080/verify/${linkId}`)
         .setColor('BLUE')
     member.send(embed)
 })
